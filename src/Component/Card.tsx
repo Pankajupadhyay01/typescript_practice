8ad7e9ea0ed4ee9884759527842743726d9da7a5
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-26 23:43:34 +0530

    display card created

--- src/Component/Card.tsx
+++ src/Component/Card.tsx
@@ -1,10 +1,35 @@
 import { weather_type } from '../Type/type'
 
- 
-const Card = (data:weather_type) => {
+
+const Card = (d: { data: weather_type; }) => {
+
+
   return (
     <div>
-      {data?.name}
+      <div className='flex flex-col justify-center m-auto items-center bg-green-100 my-2 h-[50vh] w-[80%] md:w-[40%]'>
+        {/* top row section  */}
+        <div className='flex w-[90%] justify-between my-5'>
+          <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
+            {d.data?.name}
+          </div>
+
+          <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
+            {d.data?.main.feels_like}
+          </div>
+
+          <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
+            {d.data?.sys.country}
+          </div>
+        </div>
+
+        {/* weather display  */}
+        <div className='flex justify-center items-center'>
+          <img src={`https://openweathermap.org/img/wn/${d.data?.weather[0].icon}@2x.png`} alt="" />
+        </div>
+        <div className='flex justify-between w-[90%] flex-col items-center text-lg text-blue-900 font-bold'>
+          {d.data?.main.temp}
+        </div>
+      </div>
     </div>
   )
 }
--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,41 +1,34 @@
 import axios from "axios";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { weather_type } from "../Type/type";
 import Card from "./Card";
 
 const Home = () => {
-  const [loc, setloc] = useState<string>("")
-  const [data, setdata] = useState<weather_type>()
-  const [error, seterror] = useState<boolean>(false)
+  const [loc, setloc] = useState<string>("haldwani")
+  const [data, setdata] = useState<weather_type | null>(null)
 
   const func = (e: React.ChangeEvent<HTMLInputElement>) => {
     setloc(e.target.value);
-  }
-  const get_weather = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    if (loc != "") {
-      try {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`).then(res => {
-          setdata(res.data)
-          console.log(data);
-
-        })
-      } catch (error) {
-        console.log("err");
-      }
-    } else {
-      alert("😄Oops.. you forget to enter city ")
-    }
   }
+  useEffect(() => {
+    axios
+      .get<weather_type>(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`)
+      .then(res => {
+        setdata(res.data)
+      })
+  }, [loc])
+
 
 
   return (
-    <div>
-      <form action="">
-        <input type="text" onChange={func} />
-        <button onClick={get_weather}>Submit</button>
+    <div className="flex justify-center flex-col m-auto w-full" >
+      <form onSubmit={(e: React.FormEvent<HTMLFormElement>)=>e.preventDefault()} action="" className="flex justify-center m-auto w-full">
+        <input className="h-[5vh] text-center rounded-3xl focus:border-white" type="text" onChange={func} />
       </form>
- 
+      {
+        data === null ? <div>No data </div> : <Card data={data} />
+      }
     </div>
   )
 }
