779a840f4510ca0a8fc422b0febc40d2638ef49a
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-27 13:28:22 +0530

    logic implemented

--- src/Component/Card.tsx
+++ src/Component/Card.tsx
@@ -1,35 +1,38 @@
 import { weather_type } from '../Type/type'
 
 
-const Card = (d: { data: weather_type; }) => {
-
-
+const Card = (d: { props: weather_type | null, err: string }) => {
   return (
     <div>
-      <div className='flex flex-col justify-center m-auto items-center bg-green-100 my-2 h-[50vh] w-[80%] md:w-[40%]'>
-        {/* top row section  */}
-        <div className='flex w-[90%] justify-between my-5'>
-          <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
-            {d.data?.name}
-          </div>
+      {
+        d.err == "200" ?
+          <div className='flex flex-col justify-center m-auto items-center bg-green-100 my-2 h-[50vh] w-[80%] md:w-[40%]'>
+            {/* top row section  */}
+            <div className='flex w-[90%] justify-between my-5'>
+              <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
+                {d.props?.name}
+              </div>
 
-          <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
-            {d.data?.main.feels_like}
-          </div>
+              <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
+                {d.props?.main.feels_like}
+              </div>
 
-          <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
-            {d.data?.sys.country}
-          </div>
-        </div>
+              <div className='bg-blue-500 p-3 rounded-full flex h-auto'>
+                {d.props?.sys.country}
+              </div>
+            </div>
 
-        {/* weather display  */}
-        <div className='flex justify-center items-center'>
-          <img src={`https://openweathermap.org/img/wn/${d.data?.weather[0].icon}@2x.png`} alt="" />
-        </div>
-        <div className='flex justify-between w-[90%] flex-col items-center text-lg text-blue-900 font-bold'>
-          {d.data?.main.temp}
-        </div>
-      </div>
+            {/* weather display  */}
+            <div className='flex justify-center items-center'>
+              <img src={`https://openweathermap.org/img/wn/${d.props?.weather[0].icon}@2x.png`} alt="" />
+            </div>
+            <div className='flex justify-between w-[90%] flex-col items-center text-lg text-blue-900 font-bold'>
+              {d.props?.main.temp}
+            </div>
+          </div>
+          : <div className='flex flex-col justify-center m-auto items-center bg-green-100 my-2 h-[50vh] w-[80%] text-black font-bold md:w-[40%]' >{d.err}</div>
+      }
+
     </div>
   )
 }
--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -6,16 +6,23 @@ import Card from "./Card";
 const Home = () => {
   const [loc, setloc] = useState<string>("haldwani")
   const [data, setdata] = useState<weather_type | null>(null)
-
+  const [err, seterr] = useState<string>("200")
   const func = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setloc(e.target.value);
-    e.preventDefault()
+    
+    if (e.target.value == "") {
+      setloc("haldwani")
+    } else {
+      setloc(e.target.value);
+    }
   }
   useEffect(() => {
     axios
       .get<weather_type>(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`)
       .then(res => {
+        seterr("200")
         setdata(res.data)
+      }).catch(e => {
+        seterr(e.response.data.message)
       })
   }, [loc])
 
@@ -23,12 +30,11 @@ const Home = () => {
 
   return (
     <div className="flex justify-center flex-col m-auto w-full" >
-      <form onSubmit={(e: React.FormEvent<HTMLFormElement>)=>e.preventDefault()} action="" className="flex justify-center m-auto w-full">
-        <input className="h-[5vh] text-center rounded-3xl focus:border-white" type="text" onChange={func} />
+      <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => e.preventDefault()} action="" className="flex justify-center m-auto w-full">
+        <input className="h-[5vh] text-center rounded-3xl focus:border-white" type="text" placeholder="Enter your city " onChange={func} />
       </form>
-      {
-        data === null ? <div>No data </div> : <Card data={data} />
-      }
+      <Card props={data} err={err} />
+
     </div>
   )
 }
