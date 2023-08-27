7b3ea1ffaed507084e8bbb8cf3e4c418e64c302a
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-27 23:01:11 +0530

    loading screen added

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -7,8 +7,9 @@ const Home = () => {
   const [loc, setloc] = useState<string>("haldwani")
   const [data, setdata] = useState<weather_type | null>(null)
   const [err, seterr] = useState<string>("200")
+  const [loading, setloading] = useState<boolean>(true)
   const func = (e: React.ChangeEvent<HTMLInputElement>) => {
-    
+
     if (e.target.value == "") {
       setloc("haldwani")
     } else {
@@ -21,6 +22,7 @@ const Home = () => {
       .then(res => {
         seterr("200")
         setdata(res.data)
+        setloading(false)
       }).catch(e => {
         seterr(e.response.data.message)
       })
@@ -33,7 +35,15 @@ const Home = () => {
       <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => e.preventDefault()} action="" className="flex justify-center m-auto w-full">
         <input className="h-[5vh] text-center rounded-3xl focus:border-white" type="text" placeholder="Enter your city " onChange={func} />
       </form>
-      <Card props={data} err={err} />
+
+      {
+        loading ?
+          <div className='flex flex-col justify-center m-auto items-center bg-green-100 my-2 h-[50vh] w-[80%] md:w-[40%]'>
+            Loading ..
+          </div> :
+          <Card props={data} err={err} />
+      }
+
 
     </div>
   )
